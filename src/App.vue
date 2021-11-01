372957e98b7d8728fa053a7dd06d090f309ff220
jerome <jerome.campbell@rivethealth.com>
2021-11-01 11:07:53 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav-color">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark cap-width">
+        <router-link to="/" class="navbar-brand">BYU CS 252</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <router-link to="/" class="nav-item nav-link">Home</router-link>
+            <router-link to="/policies" class="nav-item nav-link">Policies</router-link>
+            <router-link to="/schedule" class="nav-item nav-link">Schedule</router-link>
+            <!--<router-link to="/notes" class="nav-item nav-link">Class Notes</router-link>-->
+            <router-link to="/homework" class="nav-item nav-link">Homework</router-link>
+            <!--<router-link to="/ta" class="nav-item nav-link">TA Schedule</router-link>-->
+          </div>
+        </div>
+      </nav>
     </div>
     <router-view/>
+    <footer>
+      <a href="https://github.com/jmasashicampbell/CS252Redesign">
+        <img src="assets/github.png" alt="github">
+      </a>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '~bootstrap/dist/css/bootstrap.css';
+
+:root {
+  --navy-blue: #001832;
+  --dark-blue: #002d5f;
+  --royal-blue: #0062b8;
+  --dark-gray: #505050;
+  --light-gray: #c0c0c0;
+
+  --dark-shadow:
+      0 45px 80px -20px rgba(50, 50, 93, 1),
+      0 25px 40px -30px rgba(0, 0, 0, 1);
+  --light-shadow:
+      0 45px 80px -20px rgba(50, 50, 93, 0.3),
+      0 25px 40px -30px rgba(0, 0, 0, 0.35);
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 40px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  min-height: 100%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.bg-dark, .nav-color {
+  background-color: var(--navy-blue) !important;
+}
+
+.navbar-brand {
+  font-size: 1rem;
+}
+
+footer {
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--navy-blue);
+}
+
+
+footer a img{
+  height: 2rem;
+}
+
+@media only screen and (min-device-width: 641px) and (min-width: 641px){
+  html {
+    font-size: 16px;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
 }
 </style>

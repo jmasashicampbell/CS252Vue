d9c5a742ad4242c85793ac82428cc2dc7177d54e
jerome <jerome.campbell@rivethealth.com>
2021-11-02 20:47:02 -0600

    Policies page

---
+++ src/components/policy-box.vue
@@ -0,0 +1,29 @@
+<template>
+  <blue-box class="policy-box">
+    <h3>{{ title }}</h3>
+    <slot></slot>
+  </blue-box>
+</template>
+
+<script>
+import BlueBox from "./blue-box";
+export default {
+  name: "policy-box",
+  components: {BlueBox},
+  props: ["title"]
+}
+</script>
+
+<style scoped>
+
+.policy-box {
+  margin: 0.7rem 0;
+}
+
+@media only screen and (min-device-width: 641px) and (min-width: 641px) {
+  .policy-box {
+    display: inline-block;
+    width: 100%
+  }
+}
+</style>
---
+++ src/views/Policies.vue
@@ -0,0 +1,315 @@
+<template>
+  <div class="cap-width-padding">
+    <h2>CS 252 Class Policies - Fall 2021</h2>
+
+    <div class="policies">
+      <policy-box title="Objectives">
+        At the end of this course, students will be able to:
+        <ol>
+
+          <li>
+            Understand abstract models for computing machines
+            including finite automata, push-down automata, and Turing machines.
+          </li>
+
+          <li>
+            Understand common notations for defining languages
+            such as regular expressions and grammars.
+          </li>
+
+          <li>
+            Understand what kinds of problems are computable
+            and what kinds are not computable.
+          </li>
+
+          <li>
+            Understand what kinds of problems are easy to compute
+            and what kinds are hard to compute.
+          </li>
+
+          <li>
+            Think and reason carefully about computation.
+            Write about computation clearly and precisely.
+            Use rigorous methods, representations,
+            and proof techniques.
+          </li>
+
+          <li>
+            Apply theoretical concepts to practical problems
+            such as using grammars for parsing programming languages
+            and using finite automata and regular expressions
+            for string searching and pattern matching.
+          </li>
+        </ol>
+      </policy-box>
+
+      <policy-box title="Reading">
+        <p>
+          The textbook for the course is
+          <i>Introduction to the Theory of Computation</i>
+          by Michael Sipser. (ISBN: 978-1-133-18779-0)
+          We will cover much of the text following the provided schedule.
+          Please read the material for a given day prior to that day's lecture.
+        </p>
+      </policy-box>
+
+      <policy-box title="Exams">
+        <p>
+          Exams consist of a Midterm and a comprehensive Final.
+        </p>
+
+        <p>
+          The midterm exam will be available
+          for one or more normal testing days
+          as well as one or more make-up days.
+          You should plan to take the exam on one of the normal testing days.
+        </p>
+
+        <p>
+          The make-up days are reserved for situations such as
+          unexpected circumstances,
+          UAC accommodations,
+          those who forget to take the test,
+          or misunderstand the testing schedule.
+        </p>
+
+        <p>
+          Points will be deducted from your test score
+          if you take the exam on a make-up day.
+          The point deduction for taking the exam
+          on a make-up day is calculated based on
+          five points per day.
+          The deduction for the first day is five points,
+          the second day is ten points, and so on.
+        </p>
+
+        <p>
+          You must take the exam on either a normal day
+          or a make-up day, no exceptions.
+          After the end of the make-up days,
+          the exam is no longer available.
+          You need to plan take the exam on a normal testing day
+          so that if an unexpected circumstance arises
+          or you forget the test or misunderstand the schedule,
+          you will be able to complete the exam on a make-up day.
+        </p>
+
+      </policy-box>
+
+      <policy-box title="Homework">
+        <p>
+          Homework assignments are due by 10:00 pm on the due date.
+        </p>
+
+        <p>
+          Please upload a PDF file to Learning Suite containing your solutions.
+        </p>
+
+        <p>
+          Late homework is not accepted.
+        </p>
+
+        <p>
+          Email submissions are not accepted.
+        </p>
+      </policy-box>
+
+      <policy-box title="Grading">
+        <h4>
+          Grade Weighting
+        </h4>
+
+        <p>
+          Your total score will be calculated based on the following percentages:
+        </p>
+
+        <table>
+          <tr>
+            <td>Midterm Exam</td>
+            <td>33%</td>
+          </tr>
+          <tr>
+            <td>Final Exam</td>
+            <td>33%</td>
+          </tr>
+          <tr>
+            <td>Homework</td>
+            <td>34%</td>
+          </tr>
+        </table>
+        <br>
+
+
+        <h4>
+          Letter Grades
+        </h4>
+
+        <p>
+          Once your total score is calculated your letter grade will be given
+          according to the following scale:
+        </p>
+
+        <table>
+          <tr>
+            <td>A</td>
+            <td>93-100</td>
+          </tr>
+          <tr>
+            <td>A-</td>
+            <td>90-92</td>
+          </tr>
+          <tr>
+            <td>B+</td>
+            <td>87-89</td>
+          </tr>
+          <tr>
+            <td>B</td>
+            <td>83-86</td>
+          </tr>
+          <tr>
+            <td>B-</td>
+            <td>80-82</td>
+          </tr>
+          <tr>
+            <td>C+</td>
+            <td>77-79</td>
+          </tr>
+          <tr>
+            <td>C</td>
+            <td>73-76</td>
+          </tr>
+          <tr>
+            <td>C-</td>
+            <td>70-72</td>
+          </tr>
+          <tr>
+            <td>D+</td>
+            <td>67-69</td>
+          </tr>
+          <tr>
+            <td>D</td>
+            <td>63-66</td>
+          </tr>
+          <tr>
+            <td>D-</td>
+            <td>60-62</td>
+          </tr>
+          <tr>
+            <td>E</td>
+            <td>Below 60</td>
+          </tr>
+        </table>
+      </policy-box>
+
+      <policy-box title="Cheating">
+        <p>
+          All work you submit for this course must be your own individual work.
+          Do not submit work copied from someone else even if you modify it.
+          Do not allow others to copy your work.
+          If you are caught violating this policy
+          your grade for the course will be an E and
+          you will be reported to the honor code office.
+        </p>
+        <p>
+          The following are specific examples of violations of this policy
+          (note that this list is not exhaustive):
+        </p>
+
+        <ul>
+          <li>
+            Copying homework or exam answers from another student,
+            from the internet, from an answer key,
+            or from a solutions manual.
+          </li>
+
+          <li>
+            Allowing someone to copy from your homework or exam answers.
+          </li>
+
+          <li>
+            Copying solutions or parts of solutions from group-work notes or
+            TA-consultation notes.
+          </li>
+        </ul>
+      </policy-box>
+
+      <policy-box title="Honor Code Standards">
+        <p>
+          In keeping with the principles of the BYU Honor Code, students are expected
+          to be honest in all of their academic work. Academic honesty means, most
+          fundamentally, that any work you present as your own must in fact be your
+          own work and not that of another. Violations of this principle may result
+          in a failing grade in the course and additional disciplinary action by the
+          university.
+        </p>
+        <p>
+          Students are also expected to adhere to the Dress and Grooming Standards.
+          Adherence demonstrates respect for yourself and others and ensures an
+          effective learning and working environment. It is the university's
+          expectation, and my own expectation in class, that each student will abide
+          by all Honor Code standards. Please call the Honor Code Office at 422-2847
+          if you have questions about those standards.
+        </p>
+      </policy-box>
+
+      <policy-box title="Preventing Sexual Misconduct">
+        <p>
+          As required by Title IX of the Education Amendments of 1972, the
+          university prohibits sex discrimination against any participant in its
+          education programs or activities. Title IX also prohibits sexual
+          harassment including sexual violence committed by or against
+          students, university employees, and visitors to campus. As outlined in
+          university policy, sexual harassment, dating violence, domestic
+          violence, sexual assault, and stalking are considered forms of "Sexual
+          Misconduct" prohibited by the university.
+        </p>
+
+        <p>
+          University policy requires any university employee in a teaching,
+          managerial, or supervisory role to report incidents of Sexual
+          Misconduct that come to their attention through various forms
+          including face-to-face conversation, a written class assignment or
+          paper, class discussion, email, text, or social media post. If you
+          encounter Sexual Misconduct, please contact the Title IX Coordinator
+          at t9coordinator@byu.edu or 801-422-2130 or Ethics Point at
+          https://titleix.byu.edu/report-concern or 1-888-238-1062
+          (24-hours). Additional information about Title IX and resources
+          available to you can be found at titleix.byu.edu.
+        </p>
+      </policy-box>
+
+      <policy-box title="Disabilities">
+        <p>
+          BYU is committed to providing reasonable accommodation to qualified
+          persons with disabilities. If you have any disability that may
+          adversely affect your success in this course, please contact the
+          University Accessibility Center at 422-2767. Services deemed
+          appropriate will be coordinated with the student and instructor by
+          that office.
+        </p>
+      </policy-box>
+    </div>
+  </div>
+</template>
+
+<script>
+import PolicyBox from "../components/policy-box";
+export default {
+  name: "Policies",
+  components: {PolicyBox}
+}
+</script>
+
+<style scoped>
+
+.policy-box tr, td {
+  padding: 0.5rem;
+  background-color: var(--dark-blue);
+}
+
+@media only screen and (min-device-width: 641px) and (min-width: 641px) {
+  .policies {
+    column-count: 2;
+  }
+}
+</style>
